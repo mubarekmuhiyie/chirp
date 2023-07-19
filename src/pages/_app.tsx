8d5ff09725a2efad0ad2cdec7d2e55cb2394b873
mubarekmuhiyie <138660468+mubarekmuhiyie@users.noreply.github.com>
2023-07-19 16:41:25 +0300

    improved post interactivity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,15 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-  <ClerkProvider {...pageProps}>
+    <ClerkProvider {...pageProps}>
+      <Toaster position="bottom-center"/>
       <Component {...pageProps} />
-  </ClerkProvider>
-  )
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,33 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
 
-  const [ input, setInput ] = useState("");
+  const [input, setInput] = useState("");
 
-  const ctx = api.useContext()
+  const ctx = api.useContext();
 
   const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
-      setInput("")
-      void ctx.posts.getAll.invalidate()
-    }
-  })  
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post: Please try again later.");
+      }
+    },
+  });
 
   if (!user) return null;
 
@@ -42,8 +51,24 @@ const CreatePostWizard = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if(input !== "") {
+              mutate({content: input})
+            }
+          }
+        }}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+
+      {isPosting && (
+        <div className="mr-5 flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
@@ -79,27 +104,26 @@ const PostView = (props: PostWithUser) => {
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
-  if(postsLoading) return <LoadingPage/>
+  if (postsLoading) return <LoadingPage />;
 
-  if(!data) return <div>Something went wrong</div>
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <div className="flex flex-col ">
-            {data.map((fullPost) => (
-              <PostView key={fullPost.post.id} {...fullPost} />
-            ))}
-          </div>
-  )
-}
+      {data.map((fullPost) => (
+        <PostView key={fullPost.post.id} {...fullPost} />
+      ))}
+    </div>
+  );
+};
 
 export default function Home() {
-  const {isLoaded: userLoaded, isSignedIn} = useUser();
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
 
   //Start fetching asap
   api.posts.getAll.useQuery();
 
-  if(!userLoaded) return <div/>
-
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -119,7 +143,7 @@ export default function Home() {
             {isSignedIn && <CreatePostWizard />}
           </div>
 
-          <Feed/>
+          <Feed />
         </div>
       </main>
     </>
