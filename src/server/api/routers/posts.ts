c7cf817a84a7b5798c868c0770e43bcf9b099767
mubarekmuhiyie <138660468+mubarekmuhiyie@users.noreply.github.com>
2023-07-29 10:35:47 +0300

    Profile page with ssg helper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,5 @@
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
+
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import {
@@ -7,14 +7,7 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profilepicture: user.profileImageUrl,
-  };
-};
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
